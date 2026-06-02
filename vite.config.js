68a74704074ac41a1ce8b970593029fee8da5baa
Elang64 <airlanggaalisyahbanie@gmail.com>
2026-06-02 11:28:25 +0700

    fix: vite

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Portofolio-Air-Langga-Ali-Syahbani/",
+  base: "/portofolio-air-langga-ali-syahbani/",
 })
